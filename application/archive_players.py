a1d180a45df1a26adf1b5eaaf72e78994edc2cbb
Sergey Borovikov <boborovikov@yandex.ru>
2024-08-21 23:47:00 +0300

    Изменил базу данных и начал подключать её к архиву матчей

--- application/archive_players.py
+++ application/archive_players.py
@@ -2,14 +2,23 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
+from database import path_db
 import sqlite3
 
 class archive_players(Screen):
     def __init__ (self, **kwargs):
         super().__init__(**kwargs)
-        bl = BoxLayout()
+        bl = BoxLayout()       
         
+        conn = sqlite3.connect(path_db())
+        cursor = conn.cursor()
         
+        cursor.execute("SELECT * FROM players")
+        players = cursor.fetchall()
+        
+        conn.close()
+        
+        bl.add_widget(Label(text=f"Игроки: {players}"))
         
         archive = Button(text="Назад")
         archive.bind(on_release=self.switch_archive)
